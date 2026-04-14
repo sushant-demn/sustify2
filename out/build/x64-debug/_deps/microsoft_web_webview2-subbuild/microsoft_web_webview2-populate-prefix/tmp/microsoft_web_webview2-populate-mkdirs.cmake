bfca0cccf51ddd47029eed4feff5b65f07dc404a
sushant-demn <sushantpayal2200@gmail.com>
2026-04-14 15:16:59 +0530

    1

---
+++ out/build/x64-debug/_deps/microsoft_web_webview2-subbuild/microsoft_web_webview2-populate-prefix/tmp/microsoft_web_webview2-populate-mkdirs.cmake
@@ -0,0 +1,27 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file LICENSE.rst or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+# If CMAKE_DISABLE_SOURCE_CHANGES is set to true and the source directory is an
+# existing directory in our source tree, calling file(MAKE_DIRECTORY) on it
+# would cause a fatal error, even though it would be a no-op.
+if(NOT EXISTS "E:/code/Sustfy 2.0/out/build/x64-debug/_deps/microsoft_web_webview2-src")
+  file(MAKE_DIRECTORY "E:/code/Sustfy 2.0/out/build/x64-debug/_deps/microsoft_web_webview2-src")
+endif()
+file(MAKE_DIRECTORY
+  "E:/code/Sustfy 2.0/out/build/x64-debug/_deps/microsoft_web_webview2-build"
+  "E:/code/Sustfy 2.0/out/build/x64-debug/_deps/microsoft_web_webview2-subbuild/microsoft_web_webview2-populate-prefix"
+  "E:/code/Sustfy 2.0/out/build/x64-debug/_deps/microsoft_web_webview2-subbuild/microsoft_web_webview2-populate-prefix/tmp"
+  "E:/code/Sustfy 2.0/out/build/x64-debug/_deps/microsoft_web_webview2-subbuild/microsoft_web_webview2-populate-prefix/src/microsoft_web_webview2-populate-stamp"
+  "E:/code/Sustfy 2.0/out/build/x64-debug/_deps/microsoft_web_webview2-subbuild/microsoft_web_webview2-populate-prefix/src"
+  "E:/code/Sustfy 2.0/out/build/x64-debug/_deps/microsoft_web_webview2-subbuild/microsoft_web_webview2-populate-prefix/src/microsoft_web_webview2-populate-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "E:/code/Sustfy 2.0/out/build/x64-debug/_deps/microsoft_web_webview2-subbuild/microsoft_web_webview2-populate-prefix/src/microsoft_web_webview2-populate-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "E:/code/Sustfy 2.0/out/build/x64-debug/_deps/microsoft_web_webview2-subbuild/microsoft_web_webview2-populate-prefix/src/microsoft_web_webview2-populate-stamp${cfgdir}") # cfgdir has leading slash
+endif()
